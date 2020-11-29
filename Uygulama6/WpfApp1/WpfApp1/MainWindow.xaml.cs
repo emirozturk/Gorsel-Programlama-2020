105f11df7e582d091168dc232d6599f6406f3ef1
Emir Öztürk <emirozturk3@gmail.com>
2020-11-29 15:19:48 +0300

    6. Hafta Genel Tekrar Uygulama

---
+++ Uygulama6/WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    class Sayilar
+    {
+        public List<int> Liste { get; }
+        public int AltSinir { get; set; }
+        public int UstSinir { get; set; }
+        private int Aralik;
+        public Sayilar(int altSinir,int ustSinir,int aralik)
+        {
+            AltSinir = altSinir;
+            UstSinir = ustSinir;
+            Aralik = aralik;
+            Liste = new List<int>();
+            Hesapla();
+        }
+        private void Hesapla()
+        {
+            for (int i = AltSinir; i < UstSinir; i += Aralik)
+                Liste.Add(i);
+        }
+    }
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button tus = sender as Button;
+            object icerik = tus.Content;
+            string icerikString = icerik.ToString();
+            int aralik = Convert.ToInt32(icerikString);
+
+            string tbIcerigi = tbUstSinir.Text;
+            int ustSinir = Convert.ToInt32(tbIcerigi);
+
+            Sayilar sayilar = new Sayilar(0,ustSinir,aralik);
+            StringBuilder sb = new StringBuilder();
+            foreach (var eleman in sayilar.Liste)
+                sb.Append(eleman + " ");
+            MessageBox.Show(sb.ToString());
+        }
+    }
+}
---
+++ Uygulama6/WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnIslemYap_Click(object sender, RoutedEventArgs e)
+        {
+            //Ekrandan cümleyi bir şekilde elde et
+            string cumle = TbCumle.Text;
+            //Ekrandan temizlenecek string'i bir şekilde elde et
+            string temizlenecek = TbKelime.Text;
+            //alınan cümle içerisinden temizlenecek karakteri ya da stringi temizle
+            string sonuc = cumle.Replace(temizlenecek, "");
+            //sonuç metnini 3gram'lara böl
+            List<string> liste = new List<string>();
+            for (int i = 0; i < sonuc.Length; i += 3)
+                if (i + 3 < sonuc.Length)
+                    liste.Add(sonuc.Substring(i, 3));
+                else
+                    liste.Add(sonuc.Substring(i, sonuc.Length - i));
+            //elde edilen 3gram listesini ekrandaki listbox içerisinde göster
+            LbListe.ItemsSource = liste;
+        }
+    }
+}
