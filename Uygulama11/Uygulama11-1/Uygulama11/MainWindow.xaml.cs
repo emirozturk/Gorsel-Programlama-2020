5421cecc2e13550ca78bf5d225ce19207c6fad61
Emir Öztürk <emirozturk3@gmail.com>
2021-01-07 13:09:49 +0300

    Uygulama 11 Eklendi

---
+++ Uygulama11/Uygulama11-1/Uygulama11/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uygulama11
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int sayac;
+        public MainWindow()
+        {
+            InitializeComponent();
+            WpBilesenler.VerticalAlignment = VerticalAlignment.Top;
+            sayac = 0;
+        }
+        public void YeniTusEkle()
+        {
+            Button yeniTus = new Button()
+            {
+                Name = "BtnTus"+sayac,
+                Width = 100,
+                Height = 50,
+                Content = "Tuş "+sayac,
+                Margin = new Thickness(3)
+            };
+            sayac++;
+            yeniTus.Click += YeniTus_Click;
+            WpBilesenler.Children.Add(yeniTus);
+        }
+
+        private void YeniTus_Click(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            MessageBox.Show(btn.Name + " Tıklandı.");
+        }
+
+        private void BtnEkle_Click(object sender, RoutedEventArgs e)
+        {
+            YeniTusEkle();
+        }
+
+        private void BtnDegistir_Click(object sender, RoutedEventArgs e)
+        {
+            string tusIsmi = "BtnTus" + TbBoyut.Text;
+            foreach(var eleman in WpBilesenler.Children)
+                if(eleman is Button)
+                {
+                    Button btn = eleman as Button;
+                    if(btn.Name == tusIsmi)
+                    {
+                        btn.Width = 200;
+                        btn.Height = 200;
+                    }
+                }
+        }
+
+        private void BtnSil_Click(object sender, RoutedEventArgs e)
+        {
+            string tusIsmi = "BtnTus" + TbBoyut.Text;
+            Button bulunan= new Button();
+            foreach (var eleman in WpBilesenler.Children)
+                if (eleman is Button)
+                    if ((eleman as Button).Name == tusIsmi)
+                        bulunan = eleman as Button;
+            WpBilesenler.Children.Remove(bulunan);
+        }
+    }
+}
---
+++ Uygulama11/Uygulama11-2/Uygulama11-2/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uygulama11_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            KriterOku();
+        }
+
+        private void KriterOku()
+        {
+            Dictionary<string,int> kriterler = DosyadanOku();
+            BileşenOluştur(kriterler);
+        }
+
+        private void BileşenOluştur(Dictionary<string, int> kriterler)
+        {
+            foreach(var anahtar in kriterler.Keys)
+            {
+                Button btn = new Button()
+                {
+                    Content = anahtar,
+                    Width=500,
+                    Height=50,
+                    Tag = kriterler[anahtar]
+                };
+                btn.Click += Btn_Click;
+                WpBilesenler.Children.Add(btn);
+            }
+        }
+
+        private void Btn_Click(object sender, RoutedEventArgs e)
+        {
+            int deger = 0;
+            if(TbToplam.Text != "")
+                deger = Convert.ToInt32(TbToplam.Text);
+            deger += Convert.ToInt32((sender as Button).Tag);
+            TbToplam.Text = deger.ToString();
+        }
+
+        private Dictionary<string, int> DosyadanOku()
+        {
+            //var sozluk = new Dictionary<string, int>(File.ReadAllLines("kriterler.txt")
+            //                                         .Select(x => new KeyValuePair<string, int>(x.Split("-")[0], Convert.ToInt32(x.Split("-")[1]))));
+
+            var sozluk = new Dictionary<string, int>();
+            var satirlar = File.ReadAllLines("kriterler.txt");
+            foreach (var satir in satirlar)
+            {
+                string[] parcalar = satir.Split("-");
+                string kriter = parcalar[0];
+                int deger = Convert.ToInt32(parcalar[1]);
+                sozluk.Add(kriter, deger);
+            }
+            return sozluk;
+        }
+
+        private void BtnTemizle_Click(object sender, RoutedEventArgs e)
+        {
+            TbToplam.Clear();
+        }
+    }
+}
