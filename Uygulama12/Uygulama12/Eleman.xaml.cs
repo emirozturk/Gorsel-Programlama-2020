c0acb7e2ce8e6852347ae90f049665573c259ba4
Emir Öztürk <emirozturk3@gmail.com>
2021-01-14 13:00:25 +0300

    Uygulama 12 Eklendi

---
+++ Uygulama12/Uygulama12/Eleman.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Uygulama12
+{
+    public partial class Eleman : UserControl
+    {
+        public string Deger { get; set; }
+        public Eleman(string deger)
+        {
+            InitializeComponent();
+            Deger = deger;
+            LblIcerik.Content = deger;
+            BtnSola.IsEnabled = false;
+        }
+        public void YerDegistir()
+        {
+            BtnSola.IsEnabled = !BtnSola.IsEnabled;
+            BtnSaga.IsEnabled = !BtnSaga.IsEnabled;
+        }
+    }
+}
---
+++ Uygulama12/Uygulama12/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Uygulama12
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<string> liste;
+        public MainWindow()
+        {
+            InitializeComponent();
+            liste = new List<string>() { "Eleman 1", "Eleman 2", "Eleman 3", "Eleman 4", "Eleman 5" };
+            ListeleriDoldur();
+        }
+
+        private void ListeleriDoldur()
+        {
+            foreach (var deger in liste)
+            {
+                Eleman eleman = new Eleman(deger);
+                LbEklenecek.Items.Add(eleman);
+            }
+        }
+
+        private void LbEklenecek_Click(object sender, RoutedEventArgs e)
+        {
+            Eleman eleman = e.Source as Eleman;
+            LbEklenecek.Items.Remove(eleman);
+            eleman.YerDegistir();
+            LbEklenen.Items.Add(eleman);
+        }
+
+        private void LbEklenen_Click(object sender, RoutedEventArgs e)
+        {
+            Eleman eleman = e.Source as Eleman;
+            LbEklenen.Items.Remove(eleman);
+            eleman.YerDegistir();
+            LbEklenecek.Items.Add(eleman);
+        }
+    }
+}
